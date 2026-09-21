be18a10557409bcc3038eeaf2d26ea7f62705f18
Pantawan Vongvattanakun (Pall) <pantawan@live.dk>
2026-09-21 21:11:57 +0200

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/plant-webshop/">
         <App />
       </BrowserRouter>
     </Provider>
